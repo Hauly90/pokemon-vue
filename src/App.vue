5efbd9aaf2fb9851e430988321aefec0ff050c28
Jakub Macicek <jakub.macicek@plus4u.net>
2022-07-05 21:32:22 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <h1>Welcome to the Pokémon Encyclopedia!</h1>
+    <!-- <a v-if="showPokemon">{{ pokemonDetails }}</a> -->
+    <!-- <a v-if="showPokemon">{{ anotherAPICall.forms[0].name }}</a> -->
+    <div  class="d-grid gap-2 d-md-flex justify-content-center">
+      <button
+        v-on:click="showPokemon = true"
+        class="btn btn-dark"
+      >
+        Click in order to get a pokemons!
+      </button>
+      <button
+        v-on:click="showPokemon = false"
+        class="btn btn-dark"
+      >
+        Hide pokemons
+      </button>
+    </div>
+
+    <div
+      v-for="item in anotherAPICall"
+      :key="item.id"
+      v-if="showPokemon"
+      class="pokemon_rendering"
+    >
+      <img :src="item.sprites.other.dream_world.front_default" />
+      <p>{{ item.forms[0].name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Pokemon from "./components/Pokemon.vue";
+
+export default {
+  name: "App",
+  components: {
+    Pokemon,
+  },
+  data() {
+    return {
+      pokemonDetails: [],
+      anotherAPICall: [],
+      showPokemon: false,
+    };
+  },
+  methods: {
+    setAllPokemons() {
+      fetch("https://pokeapi.co/api/v2/pokemon/?limit=16")
+        .then((response) => response.json())
+        .then((data) => {
+          this.pokemonDetails = data;
+
+          let promises = data.results.map((pokemon) => fetch(pokemon.url));
+
+          let test = [];
+
+          Promise.all(promises).then((results) =>
+            results.forEach((result) => {
+              return fetch(result.url)
+                .then((data) => data.json())
+                .then((result) => test.push(result));
+            })
+          );
+
+          setTimeout(() => {
+            console.log(test);
+            this.anotherAPICall = test;
+          }, 900);
+        });
+    },
+  },
+  getAllPokemons() {},
+  created() {
+    this.setAllPokemons();
+  },
+};
+</script>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+  /* text-align: center; */
+  font-weight: normal;
+}
+
+div .pokemon_rendering {
+  border-style: solid;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin: 10px;
+  border-radius: 20px;
+  /* display: table; */
+  /* display: inline-block; */
+}
+
+h1 {
+  text-align: center;
+}
+
+button {
+  background-color: green;
+  color: white;
+  border-radius: 5px;
+  margin: 5px;
+  padding: 5px;
+}
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h3>Hello From Component</h3>
+    <h5>Received name: {{ name }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pokemon",
+
+  data() {
+    return {
+      formName: [],
+    };
+  },
+
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
